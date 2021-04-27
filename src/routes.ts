32de025f8b22176cf051cec971ad291a474f9d67
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2021-04-27 11:49:17 -0300

    Add routes file to project

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/user', (request, response) => 
+    response.json({
+        message: 'Hello, world'
+    })
+)
+
+
+export default routes;
