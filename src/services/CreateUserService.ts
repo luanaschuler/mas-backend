cf63acd692585d68c8902c03320c68e7eeb6a3a5
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2021-05-30 19:00:18 -0300

    fixes to user service

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -22,11 +22,11 @@ class CreateUserService{
 
         const hashedPassword = await hash(password, 8);
 
-        const user = {
+        const user = usersRepository.create({
             name,
             email,
             password: hashedPassword
-        }
+        })
 
         await usersRepository.save(user);
 
