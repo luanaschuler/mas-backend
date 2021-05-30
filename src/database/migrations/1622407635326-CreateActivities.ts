ded59ae61ecf2179c3f95a5cd0b15a06ad14f056
luanaschuler <66922010+luanashuler@users.noreply.github.com>
2021-05-30 17:57:05 -0300

    Feat services, controller and migrations files

---
+++ src/database/migrations/1622407635326-CreateActivities.ts
@@ -0,0 +1,41 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateActivities1622407635326 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "activities",
+                columns: [
+                    {
+                        name:"id",
+                        type:"varchar",
+                        isPrimary: true
+                    },
+                    {
+                        name:"name",
+                        type:"varchar"
+                    },
+                    {
+                        name:"activity_date",
+                        type:"date"
+                    },
+                    {
+                        name:"course_unit_id",
+                        type:"varchar"
+                    },
+                    {
+                        name:"created_at",
+                        type:"timestamp",
+                        default: "now()",
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("activities");
+    }
+
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import { UserController } from './controller/UserController';
+import { CourseUnitController } from './controller/UnitCourseController';
+import { ActivityController } from './controller/ActivityController';
+
+interface UserResquest {
+    name: string;
+    email: string;
+    password: string;
+}
+const userController = new UserController()
+const courseUnitController = new CourseUnitController();
+const activityController = new ActivityController();
 
 const routes = Router();
 
-routes.get('/user', (request, response) => 
-    response.json({
-        message: 'Hello, world'
-    })
-)
+routes.post('/user', userController.create);
+routes.post('/activity', activityController.create);
+routes.post('/courseunit', courseUnitController.create);
 
 export default routes;
